80845da3d22539baea4a176f3a85690ef1187e42
suraj rawat <surajrwat001@gmail.com>
2025-12-29 10:43:42 +0530

    initial commit

---
+++ src/redux/features/CounterSlice.js
@@ -0,0 +1,23 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const counterSlice = createSlice({
+    name: 'counter',
+    initialState: {
+        value: 10
+    },
+    reducers: {
+        increment: (state) => {
+            state.value += 1
+        },
+        decrement: (state) => {
+            state.value -= 1
+        },
+        incrementByAmount: (state,actions) => {
+            state.value += actions.payload
+        },
+    }
+})
+
+export const {increment,decrement,incrementByAmount} = counterSlice.actions
+
+export default counterSlice.reducer
